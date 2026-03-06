b981133fc9e0939b76deea1c2b0532ecaa3f07cf
Eduardo Ribeiro Alves de Oliveira <eduardoribeiro0912@gmail.com>
2026-03-06 15:27:10 -0300

    feat: implantar dados no mock de user

---
+++ src/mocks/userMock.ts
@@ -0,0 +1,8 @@
+import { type User } from "@/types/User";
+
+export const userMock: User = {
+  name: "Admin",
+  email: "admin@email.com",
+  password: "123456",
+  confirmation_password: "123456"
+};
