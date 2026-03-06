00e8ce854038c361419492b38ed3111225482f46
Eduardo Ribeiro Alves de Oliveira <eduardoribeiro0912@gmail.com>
2026-03-06 17:56:38 -0300

    feat: adicionar persistência de tema com localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react"
 import { AppRoutes } from "./routes/AppRoutes"
 
 
 function App() {
+  useEffect(() => {
+    const theme = localStorage.getItem("nexus-theme")
+
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [])
   return <AppRoutes />
 }
 
