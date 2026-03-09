6b72448caf0bbf2b93051c697b00d50c76bde31c
Eduardo Ribeiro Alves de Oliveira <eduardoribeiro0912@gmail.com>
2026-03-09 12:39:49 -0300

    feaat: criar roteamento do sistema

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,6 +6,10 @@ import { Home } from "@/pages/Home"
 
 import { DashboardLayout } from "@/layouts/DashboardLayout"
 import { ThemeProvider } from "@/context/ThemeContext"
+import { Users } from "@/pages/Users"
+import { Deposit } from "@/pages/Deposit"
+import { Conversion } from "@/pages/Conversion"
+import { Sake } from "@/pages/Sake"
 
 export function AppRoutes() {
   return (
@@ -23,6 +27,10 @@ export function AppRoutes() {
           <Route element={<DashboardLayout />}>
 
             <Route path="/home" element={<Home />} />
+            <Route path='/users' element={<Users />} />
+            <Route path="/deposit" element={<Deposit/>}/>
+            <Route path="/sake" element={<Sake/>}/>
+            <Route path="/conversion" element ={<Conversion/>}/>
 
           </Route>
 
