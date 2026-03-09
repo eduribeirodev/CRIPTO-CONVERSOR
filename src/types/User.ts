65c023410098b268e8f415a53445c4ef9a7042bf
Eduardo Ribeiro Alves de Oliveira <eduardoribeiro0912@gmail.com>
2026-03-09 11:03:48 -0300

    fix: corrigir interface de user

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,6 +1,17 @@
+export type Balance = Record<string, number>;
+
 export interface User {
-    name: string ;
-    email: string;
-    password: string;
-    confirmation_password: string; 
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  confirmation_password: string;
+  balance: Balance;
+}
+
+export interface UserRegister {
+  name: string;
+  email: string;
+  password: string;
+  confirmation_password: string;
 }
